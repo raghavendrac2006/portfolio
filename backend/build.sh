7ecd3ab09da815baddd891e2905e37c783f88104
Raghavendra Raghu <raghavendrac2006@gmail.com>
2026-07-16 12:23:14 +0530

    Auto-create superuser on build

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create a superuser automatically if environment variables are provided
+python manage.py createsuperuser --noinput || true
