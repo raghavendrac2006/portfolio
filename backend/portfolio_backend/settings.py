137ef22d5fb4033d2120ef225e6a7684d4697562
Raghavendra Raghu <raghavendrac2006@gmail.com>
2026-07-16 12:15:45 +0530

    Fix FRONTEND_URL trailing slash for CORS

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -136,4 +136,5 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 FRONTEND_URL = os.environ.get('FRONTEND_URL')
 if FRONTEND_URL:
-    CORS_ALLOWED_ORIGINS.append(FRONTEND_URL)
+    # Remove trailing slash if user accidentally added one, as django-cors-headers will reject it
+    CORS_ALLOWED_ORIGINS.append(FRONTEND_URL.rstrip('/'))
